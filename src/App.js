d9a4b8f6ab74f95852f1c138ba3d02a7a320793f
mauriciomta <mauriciomta@gmail.com>
2020-05-18 23:09:38 -0300

    edited home text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Health Track <code>Huehue</code>!
+          <code>1 + 1 = 2</code>
         </p>
         <a
           className="App-link"
